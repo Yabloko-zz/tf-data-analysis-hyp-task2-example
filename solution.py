c634634cf812a0c0daf7ee581410621d19f86a4f
Rus <iYablokoG@gmail.com>
2023-04-12 23:13:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 38897891  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from scipy.stats import cramervonmises_2samp
-    stat, p = cramervonmises_2samp(x, y)
+    from scipy.stats import anderson_ksamp
+    pval = anderson_ksamp([x, y]).pvalue
     res = p < 0.06
     return res
